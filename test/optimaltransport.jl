acddc54ad203ebc79dbde1038979f1bf725f2a80
David Widmann <david.widmann@it.uu.se>
2021-04-30 15:58:27 +0200

    Drop dependency on OptimalTransport and implement LP problem

---
+++ test/optimaltransport.jl
@@ -0,0 +1,28 @@
+@testset "optimaltransport.jl" begin
+    M = 200
+    N = 250
+    μ = rand(M)
+    ν = rand(N)
+    μ ./= sum(μ)
+    ν ./= sum(ν)
+
+    # create random cost matrix
+    C = pairwise(SqEuclidean(), rand(1, M), rand(1, N); dims=2)
+
+    # compute optimal transport map and cost with OptimalTransport.jl
+    P_ot = emd(μ, ν, C, Tulip.Optimizer())
+    cost_ot = emd2(μ, ν, C, Tulip.Optimizer())
+
+    # compute optimal transport map and squared Wasserstein-2 distance
+    lp = Tulip.Optimizer()
+    P = optimal_transport_map(μ, ν, C, lp)
+    @test size(C) == size(P)
+    @test MOI.get(lp, MOI.TerminationStatus()) == MOI.OPTIMAL
+    @test maximum(abs, P .- P_ot) < 1e-2
+
+    lp = Tulip.Optimizer()
+    cost = sqwasserstein(μ, ν, C, lp)
+    @test dot(C, P) ≈ cost atol = 1e-5
+    @test MOI.get(lp, MOI.TerminationStatus()) == MOI.OPTIMAL
+    @test cost ≈ cost_ot atol = 1e-5
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,30 @@
 using CalibrationErrorsDistributions
 using Distances
 using LinearAlgebra
+using MathOptInterface
+using OptimalTransport
 using PDMats
 using Random
 using Test
 
 using CalibrationErrorsDistributions:
-    Wasserstein, SqWasserstein, MixtureWasserstein, SqMixtureWasserstein
+    Wasserstein,
+    SqWasserstein,
+    MixtureWasserstein,
+    SqMixtureWasserstein,
+    sqwasserstein,
+    optimal_transport_map
 using Tulip: Tulip
 
 Random.seed!(1234)
 
+const MOI = MathOptInterface
+
 @testset "CalibrationErrorsDistributions" begin
+    @testset "optimal transport" begin
+        include("optimaltransport.jl")
+    end
+
     @testset "distances" begin
         @testset "Bures metric" begin
             include("distances/bures.jl")
@@ -21,7 +34,7 @@ Random.seed!(1234)
         end
     end
 
-    @testset "Kernels" begin
+    @testset "kernels" begin
         include("kernels.jl")
     end
 
